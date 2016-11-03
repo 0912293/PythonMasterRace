300b71fd0eb6b07d5b6104c831e803eb763acb88
Jamal <0880633@hr.nl>
2016-11-03 12:20:10 +0100

    Added a submitModel class which takes care if an order By statement is DESC and which column name the sql statement gets sorted by

--- src/main/java/com/steen/Controllers/RootController.java
+++ src/main/java/com/steen/Controllers/RootController.java
@@ -1,6 +1,7 @@
 package com.steen.Controllers;
 
 import com.steen.Models.SessionModel;
+import com.steen.Models.SubmitModel;
 import com.steen.session.Filter;
 import com.steen.session.Game;
 import com.steen.velocity.VelocityTemplateEngine;
@@ -27,6 +28,7 @@ public class RootController {
             return new ModelAndView(model, p_layout);
         }, new VelocityTemplateEngine());
 
+
         get("/do_something",(req,res)->{
             Map<String, Object> model = new HashMap<>();
 //            Games games = new Games();
@@ -41,27 +43,17 @@ public class RootController {
             String product = req.queryParams("search");
             String alpha = req.queryParams("alpha");
             String price = req.queryParams("pricesort");
-            String alphaOrder = "games.games_name";
-            String productFilter = "games.games_price";
-            if(price != null &&price.equals("0")){
-                productFilter = "games.games_price";
-            }
-            else{
-                productFilter = "games.games_price" + " DESC";
-            }
-            if(alpha != null && alpha.equals("0")){
-                alphaOrder = ",games.games_name";
-            }
-            else{
-                alphaOrder = ",games.games_name" + " DESC";
-            }
+
+            String alphaOrder = SubmitModel.SelectQueryColumn(alpha, "name");
+            String productFilter = SubmitModel.SelectQueryColumn(price, "price");
 
             session.getSearch().clearFilters();
             session.getSearch().clearOrderBy();
             if (!product.equals("")) {
                 session.getSearch().addFilterParam("LOWER(games_name)", "'%" + product + "%'", Filter.Operator.LIKE);
             }
-            session.getSearch().addOrderParam("" + productFilter + alphaOrder);
+            session.getSearch().addOrderParam(productFilter);
+            session.getSearch().addOrderParam(alphaOrder);
             req.session().attribute("search", product);
 
             ArrayList<Game> gameArrayList = session.getSearch().getGames();
---
+++ src/main/java/com/steen/Models/SubmitModel.java
@@ -0,0 +1,16 @@
+package com.steen.Models;
+
+/**
+ * Created by Jamal on 3-11-2016.
+ */
+public class SubmitModel {
+
+    public static String SelectQueryColumn(String desc, String column) {
+        if(desc.equals("0")){
+            return "games.games_"+column;
+        }
+        else {
+            return "games.games_"+column+" DESC";
+        }
+    }
+}
