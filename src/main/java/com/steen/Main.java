74b2b5423ccb7e1cf16cf65c113c72c7b7a14f65
Jesse Wouters <jessewouters95@gmail.com>
2017-01-16 13:14:28 +0100

    Model geupdate

--- src/main/java/com/steen/Main.java
+++ src/main/java/com/steen/Main.java
@@ -3,10 +3,7 @@ package com.steen;
 import static spark.Spark.*;
 
 import com.steen.Controllers.*;
-import com.steen.Models.AdminModel;
-import com.steen.Models.LoginModel;
-import com.steen.Models.ProductModel;
-import com.steen.Models.RegisterModel;
+import com.steen.Models.*;
 import org.apache.commons.collections.map.HashedMap;
 import spark.Spark;
 
--- src/main/java/com/steen/Models/WishlistModel.java
+++ src/main/java/com/steen/Models/WishlistModel.java
@@ -1,10 +1,58 @@
 package com.steen.Models;
 
+import com.steen.Main;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Created by jesse on 16-1-2017.
  */
 public class WishlistModel {
+    Connection connection = Main.connection;
 
-    public void test() {
-        System.out.println("Hey");
-    }}
+    private String getquery;
+    private String insertquery;
+
+    public WishlistModel(){
+
+        getResult();
+    }
+
+    public ResultSet getResult(){
+        ResultSet result = null;
+        try {
+            PreparedStatement myStmt = connection.prepareStatement(getquery);
+            result = myStmt.executeQuery();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public void insertItem(){
+        try{
+            Statement myStmt = connection.createStatement();
+            myStmt.execute(insertquery);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateInsert(){
+
+    }
+
+    public void updateGet(){
+
+    }
+}
+
+
