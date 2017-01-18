4a031eab924776baba5c290fd994d090156f85e8
lennardrk <lennardrk@outlook.com>
2017-01-18 12:54:10 +0100

    UNIT TEST:  SubmitModel test

--- src/main/java/com/steen/UnitTests/Models/SubmitModelTest.java
+++ src/main/java/com/steen/UnitTests/Models/SubmitModelTest.java
@@ -1,11 +1,13 @@
 package com.steen.UnitTests.Models;
 
+import com.steen.Models.SubmitModel;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,19 +19,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(Arquillian.class)
 public class SubmitModelTest {
+
+
     @Before
     public void setUp() throws Exception {
-
+        //Not used. selectQueryColumn method is static.
     }
 
     @After
     public void tearDown() throws Exception {
-
+        //Not used. selectQueryColumn method is static.
     }
 
     @Test
     public void selectQueryColumn() throws Exception {
-
+        Assert.assertEquals(SubmitModel.SelectQueryColumn("0", "name"), "games.games_name"); //possibility 1
+        Assert.assertEquals(SubmitModel.SelectQueryColumn("1", "name"), "games.games_name DESC"); //possibility 2
     }
 
     @Deployment
