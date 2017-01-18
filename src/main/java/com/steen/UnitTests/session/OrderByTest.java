da4a4b0482a53d4702a179123d5ea061a1fcc629
lennardrk <lennardrk@outlook.com>
2017-01-18 12:41:56 +0100

    UNIT TEST: Adding unit test skeleton for session package. Removing deprecated Game.java.

---
+++ src/main/java/com/steen/UnitTests/session/OrderByTest.java
@@ -0,0 +1,47 @@
+package com.steen.UnitTests.session;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lennard Kras on 18-1-2017.
+ */
+@RunWith(Arquillian.class)
+public class OrderByTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void addParameter() throws Exception {
+
+    }
+
+    @Test
+    public void getOrderByStatement() throws Exception {
+
+    }
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(com.steen.session.OrderBy.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+}
