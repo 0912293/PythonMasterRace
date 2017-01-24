fdca26ae0cc4265353125282acc2d1bf1a29c9bc
0912293 <Kevin181094@gmail.com>
2017-01-24 13:19:27 +0100

    Merge branch 'master' of https://github.com/0912293/PythonMasterRace into The_New_Branch
    # Conflicts:
    #	src/main/java/com/steen/models/AdminModel.java
    #	src/main/resources/public/html/games_bekijk.html
    #	src/main/resources/public/js/common.js
    #	src/main/resources/public/js/product_view.js

--- src/main/java/com/steen/models/AdminProductModel.java
+++ src/main/java/com/steen/models/AdminProductModel.java
@@ -2,20 +2,14 @@ package com.steen.models;
 
 import com.steen.Main;
 
-import com.steen.Cryptr;
-import com.steen.util.DateBuilder;
-import com.steen.Main;
-import com.steen.session.User;
-import com.steen.util.SQLToJSON;
+import com.steen.Util.SQLToJSON;
 import com.steen.session.Search;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-import static com.steen.models.LoginModel.checkBlacklist;
-import static com.steen.util.SQLToJSON.JsonListToString;
-import static com.steen.util.SQLToJSON.getFormattedResult;
+
+import static com.steen.Util.SQLToJSON.JsonListToString;
+import static com.steen.Util.SQLToJSON.getFormattedResult;
 
 public class AdminProductModel implements Model{
     private Search search = new Search("SELECT * FROM users");
