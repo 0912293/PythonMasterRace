ee8204472c49ac4e2b779ea73a54ee59fc0215ac
Jesse Wouters <jessewouters95@gmail.com>
2017-01-25 10:47:34 +0100

    Datebuilder weer toegevoegd

---
+++ src/main/java/com/steen/Util/DateBuilder.java
@@ -0,0 +1,17 @@
+package com.steen.Util;
+
+
+
+public class DateBuilder {
+
+    StringBuilder sbuilder;
+
+    public void build(String day,String month,String year){
+        sbuilder = new StringBuilder();
+        sbuilder.append(year+"-"+month+"-"+day);
+    }
+
+    public String getDate(){
+        return sbuilder.toString();
+    }
+}
