b21dcb38a9abd8c60b504173b1e5f23021db4624
0912293 <Kevin181094@gmail.com>
2017-01-25 11:42:10 +0100

    broke a button

--- src/main/resources/public/js/product_list.js
+++ src/main/resources/public/js/product_list.js
@@ -102,7 +102,7 @@ function ConstructProductbox(name, price, image, id) {
         content.append("<h5 id='game_name'>" + name + "</h5>");
         content.append("<p>Prijs: &euro;" + price + "</p></a>");
         content.append("<div class='row'><div class='col-xs-7'><a href='/games/bekijken?id="+ id +"' class='btn btn-success btn-block' role='button' id='button1'>Bekijken</a></div></div> " +
-                "<div class='row'><div class='col-xs-7'><a href='#' class='btn btn-primary btn-block' role='button'><span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'></span> Toevoegen</a>" +
+                "<div class='row'><div class='col-xs-7'><a href='#' class='btn btn-primary btn-block' role='button'  id='productListCartButton"+ id +"'><span class='glyphicon glyphicon-shopping-cart' aria-hidden='true'></span> Toevoegen</a>" +
                 "</div></div>");
         content.append("</div></div></div>");
     return content;
