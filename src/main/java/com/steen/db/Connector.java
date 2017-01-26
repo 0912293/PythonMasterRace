a3051ceed224db9a703c971d2047e55d03e73224
0912293 <Kevin181094@gmail.com>
2017-01-26 12:03:49 +0100

    added images

--- src/main/java/com/steen/db/Connector.java
+++ src/main/java/com/steen/db/Connector.java
@@ -4,18 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public final class Connector {
-  private static String url = "jdbc:mysql://localhost:3306/webshoptest";
+//  private static String url = "jdbc:mysql://localhost:3306/webshoptest";
   // Local credentials -> Lucas
 //  private static String user = "app";
 //  private static String password = "letmesee";
 
-    private static String user = "root";
-  private static String password = "root";
+//    private static String user = "root";
+//  private static String password = "root";
 
-//    private static String url = "jdbc:mysql://projectsteen.ddns.net:3306/webshopdb?&amp;allowMultiQueries=true";
-////    private static String url = "jdbc:mysql://localhost:3306/webshopdb"; //gebruiken bij deployen
-//    private static String user = "projectapp";
-//    private static String password = "HRO!1";
+    private static String url = "jdbc:mysql://projectsteen.ddns.net:3306/webshopdb?&amp;allowMultiQueries=true";
+//    private static String url = "jdbc:mysql://localhost:3306/webshopdb"; //gebruiken bij deployen
+    private static String user = "projectapp";
+    private static String password = "HRO!1";
 
     public static Connection connect(){
         try {
